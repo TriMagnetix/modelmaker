721c53736141db484177e729a1b6bb579342199b
Madison Hanberry <mhanberry1@gmail.com>
2026-04-15 16:16:45 -0700

    squash

--- modelmaker/primitives/face.py
+++ modelmaker/primitives/face.py
@@ -41,9 +41,9 @@ class Face:
 		def dedupe(points):
 			return list(set(points))
 
-		points_xy = [(round(p.x, 3), round(p.y, 3)) for p in self.points]
-		points_xz = [(round(p.x, 3), round(p.z, 3)) for p in self.points]
-		points_yz = [(round(p.y, 3), round(p.z, 3)) for p in self.points]
+		points_xy = [(p.x, p.y) for p in self.points]
+		points_xz = [(p.x, p.z) for p in self.points]
+		points_yz = [(p.y, p.z) for p in self.points]
 
 		# Try to triangulate the face in each plane before failing
 		try:
@@ -58,14 +58,15 @@ class Face:
 				hull = ConvexHull(points_yz)
 
 		# Define edges in the convex hull
-		self._calc_outline()
-		hull_edges = [
-			(
+		hull_edges = {
+			tuple({
 				tuple(self.points[s[0]]),
 				tuple(self.points[s[1]]),
-			)
+			})
 			for s in hull.simplices
-		]
+		}
+
+		self._calc_outline()
 
 		# Construct the triangles
 		self.triangles = [
@@ -76,35 +77,59 @@ class Face:
 			)
 			for s in triangulation.simplices
 		]
+		self.bad_edges = []
 
 		# Remove triangles that interfere with intended concavity
 		def is_good_triangle(triangle):
+			def is_bad_edge(e):
+				return e in hull_edges and e not in self.outline
+
+			result = True
 			p1, p2, p3 = triangle
+			edges = {
+				tuple({p1, p2}),
+				tuple({p1, p3}),
+				tuple({p2, p3}),
+			}
+			bad_edges = set(filter(is_bad_edge, edges))
+			good_edges = edges.difference(bad_edges)
 
-			def in_outline(p1, p2):
-				return (p1, p2) in self.outline or (p2, p1) in self.outline
+			if len(bad_edges) == 0: return True
 
-			def in_hull(p1, p2):
-				return (p1, p2) in hull_edges or (p2, p1) in hull_edges
+			for e in good_edges:
+				hull_edges.add(e)
+				if not is_bad_edge(e): continue
+				self.bad_edges += [e]
 
-			return not (
-				(in_outline(p1, p2) and in_outline(p2, p3) and in_hull(p3, p1))
-				or (in_outline(p1, p2) and in_outline(p3, p1) and in_hull(p2, p3))
-				or (in_outline(p2, p3) and in_outline(p3, p1) and in_hull(p1, p2))
-			)
+			self.bad_edges += bad_edges
 
-		self.triangles = list(filter(is_good_triangle, self.triangles))
+			return False
+
+		# Cull the triangles until there are no bad triangles left
+		i = 0
+		while True:
+			if i == 3: break
+			i += 1
+			updated_triangles = list(filter(is_good_triangle, self.triangles))
+
+			if len(updated_triangles) == len(self.triangles): break
+
+			self.triangles = updated_triangles
 
 	def _calc_outline(self):
 		"""
 		Creates the outline using non-shared edges in the triangulation
 		"""
 
-		self.outline = []
+		self.outline = set()
 
 		for i in range(len(self.points)):
-			edge = (
+			edge = tuple({
 				tuple(self.points[i]),
 				tuple(self.points[(i + 1) % len(self.points)]),
-			)
-			self.outline.append(edge)
+			})
+
+			if len(edge) == 1:
+				edge = (edge[0], edge[0])
+
+			self.outline.add(edge)
--- modelmaker/primitives/point.py
+++ modelmaker/primitives/point.py
@@ -1,11 +1,25 @@
 class Point:
-	def __init__(self, x, y, z):
+	def __init__(self, x, y, z=0):
 		self.x = x
 		self.y = y
 		self.z = z
 
 	def __str__(self):
 		return f"({self.x}, {self.y}, {self.z})"
+
+	def __add__(self, other):
+		return Point(
+			self.x + other.x,
+			self.y + other.y,
+			self.z + other.z,
+		)
+
+	def __sub__(self, other):
+		return Point(
+			self.x - other.x,
+			self.y - other.y,
+			self.z - other.z,
+		)
 
 	def __iter__(self):
 		for n in (self.x, self.y, self.z):
